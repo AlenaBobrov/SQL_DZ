da6b74f118a50b10d987f95d2b78be9e8cd72e31
Alena Bobrova <kaisa8721@gmail.com>
2023-06-07 19:50:29 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,26 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private static ElementsCollection cards = $$(".list__item div");
+    private static final String balanceStart = "баланс: ";
+    private static final String balanceFinish = " р.";
+    private SelenideElement upButton = $("[data-test-id=action-deposit]");
+    private SelenideElement pageRefresh = $("[data-test-id='action-reload']");
+
+    public DashboardPage() {
+
+        heading.shouldBe(visible);
+    }
+
+}
